c78d1f0bae380f009c30af424af1d120ce937847
illuminanceUA <artem.sereda1997@gmail.com>
2022-01-23 12:15:21 +0200

    save 37

--- core/base/settings/Settings.php
+++ core/base/settings/Settings.php
@@ -39,14 +39,19 @@ class Settings
         ],
     ];
 
+    private $expansion = 'core/admin/expansion/';
+
     private $defaultTable = 'teachers';
+
+    private $projectTables = [
+        'teachers' => ['name' => 'Учителя', 'img' => 'pages.png'],
+        'students' => ['name' => 'Ученики']
+    ];
 
     private $templateArr = [
       'text' => ['name', 'phone', 'address'],
       'textarea' => ['content', 'keywords']
     ];
-
-    private $expansion = 'core/admin/expansion/';
 
     static public function get($property) {
         return self::instance()->$property;
