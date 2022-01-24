56e0b2d1da259f84ccce5cfc6ac0bc7dadaf8bbd
illuminanceUA <artem.sereda1997@gmail.com>
2022-01-24 11:11:19 +0200

    save 39

--- core/admin/controller/AddController.php
+++ core/admin/controller/AddController.php
@@ -12,6 +12,8 @@ class AddController extends BaseAdmin
           $this->createTableData();
 
           $this->createOutputData();
+
+          $this->model->showForeignKeys($this->table);
     }
 
 }
