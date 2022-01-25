50b9ae1480e1c062bcd19872481987afbc737b71
illuminanceUA <artem.sereda1997@gmail.com>
2022-01-25 21:37:53 +0200

    save 56

--- libraries/FileEdit.php
+++ libraries/FileEdit.php
@@ -12,7 +12,7 @@ class FileEdit
 
     public function addFile($directory = false){
 
-        if($directory) $this->directory = $_SERVER['DOCUMENT_ROOT'] . PATH . UPLOAD_DIR;
+        if(!$directory) $this->directory = $_SERVER['DOCUMENT_ROOT'] . PATH . UPLOAD_DIR;
             else $this->directory = $directory;
 
 
@@ -59,6 +59,38 @@ class FileEdit
     }
 
     protected function createFile($file){
+
+        $fileNameArr = explode('.', $file['name']);
+        $ext = $fileNameArr[count($fileNameArr) - 1];
+        unset($fileNameArr[count($fileNameArr) - 1]);
+
+        $fileName = implode('.', $fileNameArr);
+
+        $fileName = (new TextModify())->translit($fileName);
+
+        $fileName = $this->checkFile($fileName, $ext);
+
+        $fileFullName = $this->directory . $fileName;
+
+        if($this->uploadFile($file['tmp_name'], $fileFullName))
+            return $fileName;
+
+        return false;
+    }
+
+    protected function uploadFile($tmpName, $destination){
+
+        if(move_uploaded_file($tmpName, $destination)) return true;
+
+        return false;
+    }
+
+    protected function checkFile($fileName, $ext, $fileLastName = ''){
+
+        if(!file_exists($this->directory . $fileName . $fileLastName . '.' . $ext))
+               return $fileName . $fileLastName . '.' . $ext;
+
+        return $this->checkFile($fileName, $ext, '_' . hash('crc32', time() . mt_rand(1, 1000)));
 
     }
 
