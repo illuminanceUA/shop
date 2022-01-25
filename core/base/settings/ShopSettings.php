6b1f18054e60e21cbac8c5604c0a7ac6de2f17a8
illuminanceUA <artem.sereda1997@gmail.com>
2022-01-25 18:54:14 +0200

    save 53

--- core/base/settings/ShopSettings.php
+++ core/base/settings/ShopSettings.php
@@ -3,22 +3,17 @@
 namespace core\base\settings;
 
 use core\base\controller\Singleton;
-use core\base\settings\Settings;
 
 class ShopSettings
 {
-    use Singleton{
-        instance as traitInstance;
-    }
-
-    private $baseSettings;
+    use BaseSettings;
 
     private $routes = [
         'plugins' => [
             'dir' => false,
-                'routes' => [
+            'routes' => [
 
-                ]
+            ]
         ],
     ];
 
@@ -26,31 +21,5 @@ class ShopSettings
         'text' => ['price', 'short', 'name'],
         'textarea' => ['goods_content']
     ];
-
-
-    static public function get($property) {
-        return self::instance()->$property;
-    }
-
-    static public function instance(){
-        if(self::$_instance instanceof self) {
-            return self::$_instance;
-        }
-
-
-        self::traitInstance()->baseSettings = Settings::instance();
-        $baseProperties = self::$_instance->baseSettings->clueProperties(get_class());
-        self::$_instance->setProperty($baseProperties);
-
-        return self::$_instance;
-    }
-
-    protected function setProperty($properties) {
-        if($properties) {
-            foreach ($properties as $name => $property) {
-                $this->$name = $property;
-            }
-        }
-    }
 
 }
